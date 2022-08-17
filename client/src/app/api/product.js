fe65b61df3f6298567fb3871fa678304fd9bc0c8
m-hari16 <active.hari16@gmail.com>
2022-08-17 07:10:34 +0700

    sparate backend & frotend project

--- client/src/app/api/product.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { config } from "../../config";
-
-export const getProducts = async (params) => {
-  return await axios.get(`${config.api_host}/api/product`,
-    {
-      params
-    });
-};
